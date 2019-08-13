3c499e1a119c8d6bd5666f6a27e0ade144ef1dc1
kirity <k.rapuru@gmail.com>
2019-08-13 19:40:53 +0200

    initial commit

---
+++ src/main/java/com/lendico/challenge/annuityplangenerator/AnnuityPlanGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.lendico.challenge.annuityplangenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnuityPlanGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnuityPlanGeneratorApplication.class, args);
+	}
+
+}
